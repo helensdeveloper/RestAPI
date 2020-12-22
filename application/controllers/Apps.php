4c445529f08beb7b245ad0aa02681797ec7033e5
Helens Developer <helensdeveloper@gmail.com>
2020-12-22 17:35:39 +0700

    first commit

---
+++ application/controllers/Apps.php
@@ -0,0 +1,25 @@
+<?php
+use Restserver\Libraries\REST_Controller;
+
+defined('BASEPATH') or exit('No direct script access allowed');
+require APPPATH . '/libraries/REST_Controller.php';
+class Apps extends REST_Controller
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+	public function index_get()
+	{
+		$id = $this->get('apps_id');
+		if ($id == '') {
+			$apps = $this->db->get('apps')->result();
+		} else {
+			$this->db->where('apps_id', $id);
+			$apps = $this->db->get('apps')->result();
+		}
+		$this->response($apps, REST_Controller::HTTP_OK);
+	}
+} ?>
